d7a4f8ed6c736905737efd09e28e2ce8e4fd541a
Ana Caroll <ana2417carollina@gmail.com>
2025-05-05 12:10:11 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'desafio2';
+  overallText = 'Great';
+  comparisonText = 'You scored higher than 65% of the people who have taken these tests.';
+
+  summary = [
+    { name: 'Reaction', score: 76, icon: '⚡', class: 'reaction' },
+    { name: 'Memory', score: 92, icon: '🧠', class: 'memory' },
+    { name: 'Verbal', score: 61, icon: '💬', class: 'verbal' },
+    { name: 'Visual', score: 73, icon: '👁️', class: 'visual' }
+  ];
+
+  onContinue(): void {
+    console.log('Continue clicked');
+  }
+
+  get overallScore(): number {
+    const total = this.summary.reduce((sum, item) => sum + item.score, 0);
+    return Math.round(total / this.summary.length);
+  }
 }
